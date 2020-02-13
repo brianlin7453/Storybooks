daa2918a3379520af1d091c05d2277363a070415
Brian <brianlin7453@gmail.com>
2020-02-13 18:31:56 -0500

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'',
+    googleClientID:'508470074724-unqeilsda2pdanrhvnve6to1orkegooq.apps.googleusercontent.com',
+    googleClientSecret:'ik7XT5MZdtcLbPl1hpnhaMG1'
+}
